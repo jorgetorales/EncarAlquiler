97bd26a7c2291c44b8ff242ac4c0ea054bc746bc
francben <franczore@gmail.com>
2016-02-05 23:51:39 -0300

    cambios en reservas

---
+++ EncarAlquiler/db/migrate/20160205211803_create_reservas.rb
@@ -0,0 +1,12 @@
+class CreateReservas < ActiveRecord::Migration
+  def change
+    create_table :reservas do |t|
+      t.integer :locatario_id
+      t.string :fecha_reserva
+      t.integer :monto
+
+      t.timestamps null: false
+    end
+    add_foreign_key(:reservas, :locatarios)
+  end
+end
---
+++ EncarAlquiler/db/migrate/20160205212620_create_reservas_habitaciones.rb
@@ -0,0 +1,15 @@
+class CreateReservasHabitaciones < ActiveRecord::Migration
+  def change
+    create_table :reservas_habitaciones do |t|
+      t.integer :reserva_id
+      t.integer :habitacion_id
+      t.string :fecha_inicio
+      t.string :fecha_fin
+      t.integer :precio
+
+      t.timestamps null: false
+    end
+    add_foreign_key(:reservas_habitaciones, :reservas)
+    add_foreign_key(:reservas_habitaciones, :habitaciones)
+  end
+end
